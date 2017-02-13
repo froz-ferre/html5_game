f426b91a2256c6db65f2e2a6b5607066a553ea89
Froz <alexej.ferre@gmail.com>
2017-02-13 04:15:43 +0300

    first commit

---
+++ game.js
@@ -0,0 +1,42 @@
+window.onload = init;
+
+var map;
+var ctxMap;
+
+var drawBtn;
+var clearBtn;
+
+var gameWidth = 800;
+var gameHeight = 500;
+
+var background = new Image();
+background.src = "bg.png";
+
+function init(){
+	map = document.getElementById("map");
+	ctxMap = map.getContext("2d");
+
+	map.width = gameWidth;
+	map.height = gameHeight;
+
+	drawBtn = document.getElementById("drawBtn");
+	clearBtn = document.getElementById("clearBtn");
+
+	drawBtn.addEventListener("click", drawRect, false);
+	clearBtn.addEventListener("click", clearRect, false);
+
+	drawBg();
+}
+function drawRect(){
+	ctxMap.fillStyle = "#3D3D3D";
+	ctxMap.fillRect(10, 10, 100, 100);
+}
+
+function clearRect(){
+	ctxMap.clearRect(0, 0, 800, 500);
+}
+
+function drawBg(){
+	ctxMap.drawImage(background, 0, 0, gameWidth, gameHeight,
+	0, 0, gameWidth, gameHeight);
+}
